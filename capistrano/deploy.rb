abdd1604f5b4c6a9224e054a4ecfd8fd66e234f6
Chakir Qatab <javachakir@gmail.com>
2018-06-19 21:26:23 +0400

    add ssh 5

--- capistrano/deploy.rb
+++ capistrano/deploy.rb
@@ -12,7 +12,7 @@ set :application, 'yii-application'
 set :repo_url, 'https://github.com/chakAs3/yii-application.git'
 
 # Define where to put your application code
-set :deploy_to, "/var/www/my_app"
+set :deploy_to, "/var/www"
 
 set :pty, true
 
