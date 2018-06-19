b9b72c06c381a6076a753481956b23d240da1572
Chakir Qatab <javachakir@gmail.com>
2018-06-19 22:07:52 +0400

    deployer chmod

--- capistrano/stages/prod.rb
+++ capistrano/stages/prod.rb
@@ -12,3 +12,14 @@ server '206.189.102.195', user: 'deployer' , roles: %w{app}
 # SSH Options
 # See the example commented out section in the file
 # for more options.
+namespace :system do
+desc "Install Composer"
+    task :install_composer do
+      on roles(:app) do
+        within shared_path  do
+            execute "php -r \"copy('http://getcomposer.org/composer.phar', '#{shared_path}/composer.phar');\" 1>&2"
+        end
+      end
+    end
+
+end
