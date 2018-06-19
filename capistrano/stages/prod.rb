6015b2bda6c9179e4313cadf66cec158065d0391
Chakir Qatab <javachakir@gmail.com>
2018-06-19 21:49:51 +0400

    deployer chmod

--- capistrano/stages/prod.rb
+++ capistrano/stages/prod.rb
@@ -1,13 +1,13 @@
 # Define roles, user and IP address of deployment server
 # role :name, %{[user]@[IP adde.]}
 set :ssh_options, { :forward_agent => true }
-role :app, %w{root@206.189.102.195}
+role :app, %w{deployer@206.189.102.195}
 
 # Define server(s)
 # Example:
 # server '[your droplet's IP addr]', user: '[the deployer user]', roles: %w{[role names as defined above]}
 # server '162.243.74.190', user: 'deployer', roles: %w{app}
-server '206.189.102.195', user: 'root' , roles: %w{app}
+server '206.189.102.195', user: 'deployer' , roles: %w{app}
 
 # SSH Options
 # See the example commented out section in the file
