d25bd30179082decc370b779ec03a2fd0698dc18
Chakir Qatab <javachakir@gmail.com>
2018-06-19 20:12:15 +0400

    add ssh

--- capistrano/stages/prod.rb
+++ capistrano/stages/prod.rb
@@ -1,5 +1,6 @@
 # Define roles, user and IP address of deployment server
 # role :name, %{[user]@[IP adde.]}
+set :ssh_options, { :forward_agent => true }
 role :app, %w{deployer@206.189.102.195}
 
 # Define server(s)
@@ -11,7 +12,4 @@ server '206.189.102.195', user: 'deployer' , roles: %w{app}
 # SSH Options
 # See the example commented out section in the file
 # for more options.
-set :ssh_options, {
-    forward_agent: true,
-    user: 'deployer',
-}
+user 'deployer'
